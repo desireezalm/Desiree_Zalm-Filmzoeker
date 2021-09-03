293715a01f600b28c4d03766d870e99e1fa98dfa
Desiree Zalm <d.zalm@outlook.com>
2021-09-03 17:16:35 +0200

    Add function 'addMoviesToDom'.

--- JS/script.js
+++ JS/script.js
@@ -4,34 +4,38 @@
 import { movies } from './movies.js';
 
 
-// Add movies to DOM
 
-const movieList = document.getElementById("movieDatabase");
-
-/*
+// GET MOVIE TITLES
 let movieTitles = movies.map ((movie) => {
     return movie.Title;
 });
-console.log(movieTitles);
-*/
+// console.log(movieTitles);
 
+
+// GET MOVIE POSTERS
 let moviePoster = movies.map ((movie) => {
     return movie.Poster;
 });
 // console.log(moviePoster);
 
+
 /*
-const addMoviesToDom = () => {
+const addMovieTitlesToDom = () => {
     for(let i=0; i<movieTitles.length; i++) {
-        let newMovie = movieTitles[i];
-        let entry = document.createElement("li");
-        entry.innerHTML = newMovie;  
-        movieList.appendChild(entry);
+        let newMovieTitle = movieTitles[i];
+        let entryTitle = document.createElement("li");
+        entryTitle.classList.add("entryMovie");
+        entryTitle.innerHTML = newMovieTitle;  
+        movieList.appendChild(entryTitle);
+        entryTitle.appendChild
     }
 }
-addMoviesToDom();
-*/
+addMovieTitlesToDom();
 
+
+//const movieEntry = document.getElementsByClassName("entryMovie")
+*/
+/*
 const addMoviePosters = () => {
     for(let i=0; i<moviePoster.length; i++) {
         let newPoster = moviePoster[i];
@@ -41,5 +45,23 @@ const addMoviePosters = () => {
     }
 }
 addMoviePosters();
+*/
 
+
+// Add movies to DOM
+
+const movieList = document.getElementById("movieDatabase");
+
+const addMoviesToDom = () => {
+    for(let i=0; i<moviePoster.length; i++) {
+        let entry = moviePoster[i];
+        let entryTitle = document.createElement("li");
+        entryTitle.classList.add("entryMovie");
+        let entryPoster = document.createElement("img");
+        entryPoster.src = entry; 
+        movieList.appendChild(entryTitle);
+        entryTitle.appendChild(entryPoster);
+    }
+}
+addMoviesToDom();
 
